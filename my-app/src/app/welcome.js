05313996a952be66dd0358665499f28cc98cd29e
wuddaeng <testsuhan@gmail.com>
2023-10-30 23:51:39 +0900

    10/30

---
+++ my-app/src/app/welcome.js
@@ -0,0 +1,9 @@
+import Header from './page.module.css'
+export default function Welcome(){
+return (
+	<div>
+	<Header/> 
+		//흔히 말하는 컴포넌트(부품)입니다.이것이 리엑트 라이브러리의 장점이에요 컴포넌트로 파일을 넣을수 있어요. 물론 헤더는 직접 구현 해야 합니다.쬐끔 고민좀 해야 할거에요
+	</div>
+)
+}
