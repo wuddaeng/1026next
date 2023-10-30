7a253ec0bb126e5fb3ed0b7ca8dfa9c18c96fc01
JunMango <gudwns4985@gmail.com>
2023-10-31 01:42:21 +0900

    welcomePage & fadeIn

---
+++ my-app/src/app/welcome/page.js
@@ -0,0 +1,16 @@
+import Header from "@/component/header";
+import "../../styles/welcome.css";
+export default function Welcome(){
+    return (
+        <div>
+          {/*<Header/>*/}
+            <div className="welcome">
+                <div className="welcome-text-wrap">
+                    <h1>
+                        welcome Page
+                    </h1>
+                </div>
+            </div>
+        </div>
+    )
+}
