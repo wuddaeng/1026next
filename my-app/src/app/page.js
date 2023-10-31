546908b6e5e8107d961e6e57c9cf05b63584b235
wuddaeng <testsuhan@gmail.com>
2023-10-31 22:53:48 +0900

    copy

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -1,14 +1,20 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import Link from "next/link";
+import  "../styles/button.css"
 
 export default function Home() {
+    const welcomeURL  = "/welcome"
   return (
     <main className={styles.main}>
-
-
-
-        <span className="tip1">Welcome</span>
-
+        <Link href={welcomeURL}>
+        <button className="button btn-1">
+            go to Welcome page
+        </button>
+        </Link>
+        <Link href={welcomeURL}>
+        <button className="tip1">Welcome</button>
+        </Link>
         <span className="tip2">Bye</span>
     </main>
   )
---
+++ my-app/src/app/welcome/page.js
@@ -0,0 +1,24 @@
+import Header from "@/component/header";
+import "../../styles/welcome.css";
+export default function Welcome(){
+    return (
+        <div>
+          {/*<Header/>*/}
+            <div className="welcome">
+                <div className="welcome-text-wrap">
+                    <h1>
+                        welcome Page
+                    </h1>
+                </div>
+
+                <div className="imtrash">
+                    <h1>
+                        Im trash
+                    </h1>
+                </div>
+                
+            </div>
+
+        </div>
+    )
+}
